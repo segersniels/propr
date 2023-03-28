142905ced2ed2a45c512ddbaf3787aa1e4c5d898
segersniels <segers.n@hotmail.com>
2023-03-28 21:04:27 +0200

    :mag: Refactor meta tag descriptions for SEO optimization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const description =
+    'Why wait until GitHub Copilot X is here? Generate proper PR descriptions using ChatGPT!';
+
   return (
     <Html lang="en">
       <Head>
@@ -9,22 +12,13 @@ export default function Document() {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝</text></svg>"
         />
 
-        <meta
-          name="description"
-          content="Generate proper pull request descriptions"
-        />
+        <meta name="description" content={description} />
         <meta property="og:site_name" content="propr.dev" />
-        <meta
-          property="og:description"
-          content="Generate proper pull request descriptions"
-        />
+        <meta property="og:description" content={description} />
         <meta property="og:title" content="Propr" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Propr" />
-        <meta
-          name="twitter:description"
-          content="Generate proper pull request descriptions"
-        />
+        <meta name="twitter:description" content={description} />
 
         <meta property="og:image" content="https://propr.dev/og-image.png" />
         <meta name="twitter:image" content="https://propr.dev/og-image.png" />
