40d8aa0d34e015c4e5d64e2e1ab7c74627fa9ad1
segersniels <segers.n@hotmail.com>
2023-03-28 21:49:52 +0200

    :children_crossing: Improve prompt quality

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,5 +1,9 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { generate } from 'lib/api';
+import { NextRequest } from 'next/server';
+import { OpenAIStream, OpenAIStreamPayload } from 'helpers/Stream';
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('Missing env var from OpenAI');
+}
 
 export const config = {
   runtime: 'edge',
@@ -9,13 +13,20 @@ export default async function handler(req: NextRequest) {
   const body = await req.json();
   const prompt = `
     Generate a concise PR description from the provided git diff according to a provided template.
-    Be thorough and concise. The PR description should be a good summary of the changes made. Don't be afraid to go into detail.
+    Be thorough and concise. The PR description should be a good summary of the changes made.
+
+    Do not reference each file and function added but rather give a general explanation of the changes made.
+    Don't mention each change individually, but rather group them together.
+
+    You are free to make a calculated guess as to which changes and files are related to each other so you can group them together.
+    When endpoints/routes are in the diff try to reference these when describing features.
+
+    It's not worth mentioning that you added tests when you mention you added a new feature as it implies that you added tests in the first place.
+
+    If notes or reason why the change happened are requested, make sure you try to explain the reasoning without using too much technical jargon.
     If a section has no content, you can leave the entire section out.
 
-    It's not worth mentioning that you added tests when you mention you added a new feature.
-    It implies that you added tests in the first place.
-
-    Here is the template: """
+    The PR description should be structured as follows: """
     ${body.template}
     """
 
@@ -24,9 +35,17 @@ export default async function handler(req: NextRequest) {
     """
   `;
 
-  const message = await generate(prompt);
-
-  return NextResponse.json({
-    message,
+  const stream = await OpenAIStream({
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: prompt }],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 200,
+    stream: true,
+    n: 1,
   });
+
+  return new Response(stream);
 }
