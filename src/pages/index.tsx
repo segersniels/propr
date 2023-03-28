12c65d2d402971d5e5d6653767dc3c479d54f07c
segersniels <segers.n@hotmail.com>
2023-03-28 20:50:04 +0200

    :sparkles: Add step-by-step instructions for generating a PR description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,25 @@ import {
 import styles from 'styles/Home.module.css';
 import Footer from 'components/Footer';
 import { AiOutlineLoading } from 'react-icons/ai';
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown';
+import Step from 'components/Step';
+
+const DIFF_PLACEHOLDER = `diff --git a/.docker/cassandra/Dockerfile b/.docker/cassandra/Dockerfile
+new file mode 100644
+index 0000000000..2d20e27312
+--- /dev/null
++++ b/.docker/cassandra/Dockerfile
+@@ -0,0 +1,6 @@
++FROM cassandra:3.11
++
++COPY entrypoint.sh /entrypoint.sh
++
++ENTRYPOINT ["/entrypoint.sh"]
++CMD ["cassandra", "-f"]`;
+
+const TEMPLATE_PLACEHOLDER = `### Added
+### Removed
+### Fixed`;
 
 const TextArea = (
   props: DetailedHTMLProps<
@@ -90,21 +108,30 @@ export default function Home() {
       </Head>
 
       <div className={styles.wrapper}>
+        <p className={styles.subtitle}>
+          Why wait until GitHub Copilot X is here...
+        </p>
         <h1 className={styles.title}>
-          Generate proper PR descriptions 📝
+          Generate your next Pull Request description using ChatGPT
         </h1>
 
-        <form onSubmit={handleSubmit}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <Step step={1}>Navigate to your PR on GitHub</Step>
+          <Step step={2}>Add `.diff` to the end of the URL</Step>
+          <Step step={3}>Copy paste 🚀</Step>
+
           <TextArea
             value={diff}
-            placeholder="Paste your diff here"
+            placeholder={DIFF_PLACEHOLDER}
             onChange={(event) => setDiff(event.target.value)}
             rows={10}
           />
 
+          <Step step={4}>Provide a template</Step>
+
           <TextArea
             value={template}
-            placeholder="Provide a template"
+            placeholder={TEMPLATE_PLACEHOLDER}
             onChange={(event) => setTemplate(event.target.value)}
             rows={4}
           />
