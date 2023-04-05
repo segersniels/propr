7d95f2d0eeaba43484b423b4091097d233c5574d
segersniels <segers.n@hotmail.com>
2023-04-05 09:14:52 +0200

    :bento: Add favicon instead of using emoji

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,10 +7,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📝</text></svg>"
-        />
+        <link rel="icon" href="/favicon.ico" />
 
         <meta name="description" content={description} />
         <meta property="og:site_name" content="propr.dev" />
