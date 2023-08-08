4838e9061c1949949a4b49519ba22df073e66f10
segersniels <segers.n@hotmail.com>
2023-08-08 15:00:43 +0200

    :lipstick: Adjust title to be more friendly for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,8 +58,8 @@ export default function Page() {
   return (
     <div className="flex flex-row mx-auto items-center justify-center p-4 min-h-screen">
       <div className="flex flex-col flex-1 max-w-xl w-full items-center">
-        <h1 className="text-7xl font-bold text-center tracking-tighter mb-8">
-          Generate your next proper PR description using ChatGPT
+        <h1 className="text-5xl md:text-7xl font-bold text-center tracking-tighter mb-8">
+          Generate your next proper PR description
         </h1>
 
         <form
