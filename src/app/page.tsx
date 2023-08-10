ea1d2f6e1348458cdd4b46cb477e1652f2e8cfb1
segersniels <segers.n@hotmail.com>
2023-08-10 13:07:57 +0200

    :lipstick: Make title bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Form from 'components/form';
 export default function Page() {
   return (
     <div className="flex max-w-prose flex-col items-center justify-center p-4 md:py-24">
-      <h1 className="text-5xl md:text-6xl font-bold text-center tracking-tighter mb-8">
+      <h1 className="text-5xl md:text-7xl font-bold text-center tracking-tighter mb-8">
         Generate your next proper PR description
       </h1>
 
