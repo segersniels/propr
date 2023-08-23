9b14b06a830314bb344e0ad97c78c376ba64192d
segersniels <segers.n@hotmail.com>
2023-08-23 12:04:07 +0200

    :mag: Improve SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,14 +8,21 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Propr',
   description:
-    'Why wait until GitHub Copilot X is here? Generate proper PR descriptions now using AI!',
+    'Why wait until copilot for pull requests is here? Generate proper PR descriptions based on your git diff now with the power of AI!',
   icons: '/favicon.ico',
   twitter: {
     card: 'summary_large_image',
     title: 'Propr',
     description:
-      'Why wait until GitHub Copilot X is here? Generate proper PR descriptions now using AI!',
+      'Why wait until copilot for pull requests is here? Generate proper PR descriptions based on your git diff now with the power of AI!',
     images: ['https://propr.dev/og-image.png'],
+  },
+  openGraph: {
+    title: 'Propr',
+    description:
+      'Why wait until copilot for pull requests is here? Generate proper PR descriptions based on your git diff now with the power of AI!',
+    images: ['https://propr.dev/og-image.png'],
+    url: 'https://propr.dev',
   },
 };
 
