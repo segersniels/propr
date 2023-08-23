3fb171cb6547fe1721a50c7172dddc76f6a64856
segersniels <segers.n@hotmail.com>
2023-08-23 12:20:47 +0200

    :speech_balloon: Change title slightly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Form from 'components/form';
 export default function Page() {
   return (
     <div className="flex max-w-prose flex-col items-center justify-center p-4 md:py-24">
-      <h1 className="text-5xl md:text-7xl font-bold text-center tracking-tighter mb-8">
-        Generate your next proper PR description
+      <h1 className="text-5xl sm:text-7xl font-bold text-center tracking-tighter mb-8">
+        Properly generate your next pull request.
       </h1>
 
       <Form />
