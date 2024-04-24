0c87bc507f2e5630a8337311a45e1b06b74dd34f
segersniels <segers.n@hotmail.com>
2024-04-24 12:18:17 +0200

    :tada: Add new CLI implementation

---
+++ README.md
@@ -0,0 +1,52 @@
+# Propr
+
+![GitHub Workflow Status (with event)](https://img.shields.io/github/actions/workflow/status/segersniels/propr/ci.yml)
+
+Generate GitHub PR descriptions from the command line with the help of AI.
+`propr` aims to populate a basic PR description right from your terminal so you can focus on more important things.
+
+<p align="center">
+<img src="./resources/logo.png" width="250">
+
+## Install
+
+```bash
+# Install in the current directory
+curl -sSL https://raw.githubusercontent.com/segersniels/propr/master/scripts/install.sh | bash
+# Install in /usr/local/bin
+curl -sSL https://raw.githubusercontent.com/segersniels/propr/master/scripts/install.sh | sudo bash -s /usr/local/bin
+```
+
+### Manual
+
+1. Download the latest binary from the [releases](https://github.com/segersniels/propr/releases/latest) page for your system
+2. Rename the binary to `propr`
+3. Copy the binary to a location in your `$PATH`
+
+## Usage
+
+```
+NAME:
+   propr - Generate your PRs from the command line with AI
+
+USAGE:
+   propr [global options] command [command options]
+
+VERSION:
+   x.x.x
+
+COMMANDS:
+   init      Initializes propr with a base configuration
+   create    Creates a PR with a generated description
+   generate  Generates a PR description and outputs it
+   config    Configure propr to your liking
+   help, h   Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --help, -h     show help
+   --version, -v  print the version
+```
+
+### Using an assistant
+
+You can configure `propr` to use a custom assistant using `propr init`. This may be interesting if you want to share prompts and templates between devices and/or want to control everything externally.
---
+++ propr.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/huh/spinner"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/log"
+	"github.com/google/go-github/v61/github"
+)
+
+type Propr struct {
+	client OpenAI
+}
+
+func NewPropr() *Propr {
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	if apiKey == "" {
+		log.Fatal("OPENAI_API is not set")
+	}
+
+	return &Propr{
+		client: *NewOpenAI(apiKey),
+	}
+}
+
+func (p *Propr) Generate() (string, error) {
+	branch, err := getDefaultBranch()
+	if err != nil {
+		return "", err
+	}
+
+	diff, err := getDiff(branch)
+	if err != nil {
+		return "", err
+	}
+
+	if diff == "" {
+		return "", fmt.Errorf("no diff found")
+	}
+
+	var description string
+	err = spinner.New().TitleStyle(lipgloss.NewStyle()).Title("Generating your pull request...").Action(func() {
+		if CONFIG.Data.Assistant.Enabled {
+			response, err := p.client.GetAssistantCompletion(diff)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			description = response
+		} else {
+			response, err := p.client.GetChatCompletion(diff)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			description = response
+		}
+	}).Run()
+
+	if err != nil {
+		return "", err
+	}
+
+	return description, nil
+}
+
+func (p *Propr) Create(description string) error {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		log.Fatal("GITHUB_TOKEN is not set")
+	}
+
+	info, err := getRepositoryInformation()
+	if err != nil {
+		return err
+	}
+
+	client := github.NewClient(nil).WithAuthToken(token)
+	repository, _, err := client.Repositories.Get(context.Background(), info.Owner, info.Name)
+	if err != nil {
+		return err
+	}
+
+	var title string
+	err = huh.NewInput().Title("Provide a title for your pull request").Value(&title).Run()
+	if err != nil {
+		return nil
+	}
+
+	base := repository.GetDefaultBranch()
+	head, err := getCurrentBranch()
+	if err != nil {
+		return err
+	}
+
+	log.Debug("Creating pull request", "head", head, "base", base)
+	pr, response, err := client.PullRequests.Create(context.Background(), info.Owner, info.Name, &github.NewPullRequest{
+		Head:  github.String(head),
+		Base:  github.String(base),
+		Title: github.String(title),
+		Body:  github.String(description),
+	})
+
+	if err != nil {
+		return err
+	} else if response.StatusCode != 201 {
+		log.Fatal("failed to create pull request", "error", response.Status)
+	}
+
+	fmt.Printf("pull request created at %s\n", pr.GetHTMLURL())
+
+	return nil
+}
